4a269fdb64d49ec2826d3da1153387a0624e47c3
h3yon <khykhy1006@gmail.com>
2021-10-14 20:18:56 +0900

    Chapter3 Customized JPA

--- chapter3/src/main/java/tacos/data/OrderRepository.java
+++ chapter3/src/main/java/tacos/data/OrderRepository.java
@@ -1,8 +1,25 @@
 package tacos.data;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
 import tacos.Taco;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
+
+
+//    List<Order> findByDeliveryZip(String deliveryZip);
+//    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(
+//            String deliveryZip, Date startDate, Date endDate
+//    );
+//    List<Order> findByDeliveryToAndDeliveryCityAllIgnoresCase(String deliveryTo, String deliveryCity);
+//    List<Order> findByDeliveryCityOrderByDeliveryTo(String city);
+//
+//    @Query("Order o where o.deliveryCity='Seattle'")
+//    List<Order> readOrdersDeliveredInSeattle();
+
+
 }
