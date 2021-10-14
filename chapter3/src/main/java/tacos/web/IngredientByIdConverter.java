0b3ba27edd9a745831e73c6fffc49deddfddf34b
h3yon <khykhy1006@gmail.com>
2021-10-14 19:59:36 +0900

    Chapter3 Jdbc

---
+++ chapter3/src/main/java/tacos/web/IngredientByIdConverter.java
@@ -0,0 +1,24 @@
+package tacos.web;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import tacos.Ingredient;
+import tacos.data.IngredientRepository;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private IngredientRepository ingredientRepo;
+
+    public IngredientByIdConverter(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
+    @Override
+    public Ingredient convert(String id) { // String to Ingredient
+        return ingredientRepo.findById(id);
+    }
+
+}
