3988305b28272dac6d749411d682501c35058625
h3yon <khykhy1006@gmail.com>
2021-10-15 11:56:42 +0900

    Chapter4 JDBC Security

--- chapter4/tacos/src/main/java/tacos/security/SecurityConfig.java
+++ chapter4/tacos/src/main/java/tacos/security/SecurityConfig.java
@@ -1,14 +1,22 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/design", "/orders")
@@ -19,7 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /* 1. 인메모리 방식
         auth.inMemoryAuthentication()
                 .withUser("user1")
                 .password("{noop}password1")
@@ -28,5 +37,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user2")
                 .password("{noop}password2")
                 .authorities("ROLE_USER");
+        */
+
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                // 스프링 시큐리티와 다른 데이터베이스(테이블이나 열의 이름이 다를 때)를 사용할 경우
+                .usersByUsernameQuery("select username, password, enabled, from users where username=?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username=?")
+                .passwordEncoder(new NoEncodingPasswordEncoder());
     }
 }
