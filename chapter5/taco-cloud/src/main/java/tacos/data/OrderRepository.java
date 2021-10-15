c2d133e79300bc400bcf35862ba4dec273ebcbe9
h3yon <khykhy1006@gmail.com>
2021-10-15 16:11:30 +0900

    Chapter5 Api Paging

--- chapter5/taco-cloud/src/main/java/tacos/data/OrderRepository.java
+++ chapter5/taco-cloud/src/main/java/tacos/data/OrderRepository.java
@@ -1,25 +1,15 @@
 package tacos.data;
 
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
-import tacos.Taco;
+import tacos.User;
 
-import java.util.Date;
 import java.util.List;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
-
-//    List<Order> findByDeliveryZip(String deliveryZip);
-//    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(
-//            String deliveryZip, Date startDate, Date endDate
-//    );
-//    List<Order> findByDeliveryToAndDeliveryCityAllIgnoresCase(String deliveryTo, String deliveryCity);
-//    List<Order> findByDeliveryCityOrderByDeliveryTo(String city);
-//
-//    @Query("Order o where o.deliveryCity='Seattle'")
-//    List<Order> readOrdersDeliveredInSeattle();
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 
 
 }
--- chapter5/taco-cloud/src/main/java/tacos/web/OrderController.java
+++ chapter5/taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -1,8 +1,11 @@
 package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
@@ -42,15 +45,19 @@ public class OrderController {
             , SessionStatus sessionStatus
             , @AuthenticationPrincipal User user
     ) {
-        if (errors.hasErrors()) {
-            return "orderForm";
-        }
+        if (errors.hasErrors()) return "orderForm";
 
         order.setUser(user);
-
         orderRepo.save(order);
         sessionStatus.setComplete();
 
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model){
+        Pageable pageable = PageRequest.of(0, 20);
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 }
