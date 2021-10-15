cf790f98786411b4b88ab308d37ff08050e28ae2
h3yon <khykhy1006@gmail.com>
2021-10-15 12:31:42 +0900

    Chapter4 LDAP Security

--- chapter4/tacos/src/main/java/tacos/security/SecurityConfig.java
+++ chapter4/tacos/src/main/java/tacos/security/SecurityConfig.java
@@ -1,19 +1,24 @@
 package tacos.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation
+        .authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation
+        .web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web
+        .configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web
+        .configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-import javax.sql.DataSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -39,12 +44,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorities("ROLE_USER");
         */
 
-        auth
-                .jdbcAuthentication()
+        /* 2. LDAP 기반 사용자 스토어
+        auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 // 스프링 시큐리티와 다른 데이터베이스(테이블이나 열의 이름이 다를 때)를 사용할 경우
                 .usersByUsernameQuery("select username, password, enabled, from users where username=?")
                 .authoritiesByUsernameQuery("select username, authority from authorities where username=?")
                 .passwordEncoder(new NoEncodingPasswordEncoder());
+        */
+
+        auth.ldapAuthentication()
+                .userSearchBase("ou=people")
+                .userSearchFilter("(uid={0})")
+                .groupSearchBase("ou=groups")
+                .groupSearchFilter("member={0}")
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("userPassword");
+                /*
+                .contextSource()
+                // 아래 둘 중 하나
+                .url("ldap://tacocloud.com:389/dc=tacoclud,dc=com")
+                .root("dc=tacocloud,dc=com")
+                //LDAP 데이터를 나타내는 표준화된 방법
+                .ldif("classpath:user.ldif")
+                 */
     }
 }
