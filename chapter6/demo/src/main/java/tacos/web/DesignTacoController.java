216dfc6efaed5476a343bce8a3eb4c509e9e4a76
h3yon <khykhy1006@gmail.com>
2021-10-15 18:13:10 +0900

    Chapter6.2.1 HyperLink(Not HardCoding)

--- chapter6/demo/src/main/java/tacos/web/DesignTacoController.java
+++ chapter6/demo/src/main/java/tacos/web/DesignTacoController.java
@@ -8,6 +8,7 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,26 +33,30 @@ public class DesignTacoController {
     }
 
     @GetMapping("/recent")
-    public CollectionModel<EntityModel<Taco>> recentTacos(){
-        PageRequest page=PageRequest.of(0, 12,Sort.by("createdAt").descending());
+    public CollectionModel<EntityModel<Taco>> recentTacos() {
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
 
-        List<Taco> tacos=tacoRepo.findAll(page).getContent();
-        CollectionModel<EntityModel<Taco>> recentResources= CollectionModel.wrap(tacos);
+        List<Taco> tacos = tacoRepo.findAll(page).getContent();
+        CollectionModel<EntityModel<Taco>> recentResources = CollectionModel.wrap(tacos);
 
-        recentResources.add(new Link("http://localhost:8080/design/recent","recents"));
+        recentResources.add(
+                WebMvcLinkBuilder
+                        .linkTo(WebMvcLinkBuilder.methodOn(DesignTacoController.class)
+                                .recentTacos())
+                        .withRel("recents"));
         return recentResources;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id){
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
         Optional<Taco> optTaco = tacoRepo.findById(id);
-        if(optTaco.isPresent()) return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        if (optTaco.isPresent()) return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping(consumes="application/json")
+    @PostMapping(consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public Taco postTaco(@RequestBody Taco taco){
+    public Taco postTaco(@RequestBody Taco taco) {
         return tacoRepo.save(taco);
     }
 
