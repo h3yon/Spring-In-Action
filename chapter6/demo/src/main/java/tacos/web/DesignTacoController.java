2e9c5a5fa55697c336a2afa9cf46f61b30ec5608
h3yon <khykhy1006@gmail.com>
2021-10-15 17:54:33 +0900

    Chapter6 Rest Controller

--- chapter6/demo/src/main/java/tacos/web/DesignTacoController.java
+++ chapter6/demo/src/main/java/tacos/web/DesignTacoController.java
@@ -1,87 +1,50 @@
 package tacos.web;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import tacos.Ingredient;
-import tacos.Ingredient.Type;
 import tacos.Order;
 import tacos.Taco;
-import tacos.User;
-import tacos.data.IngredientRepository;
 import tacos.data.TacoRepository;
-import tacos.data.UserRepository;
 
-import javax.validation.Valid;
-import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
-@Slf4j
-@Controller
-@RequestMapping("/design")
-@SessionAttributes("order")
+@RequestMapping(value = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+@RestController
 public class DesignTacoController {
 
-    private final IngredientRepository ingredientRepo;
     private TacoRepository tacoRepo;
-    private UserRepository userRepo;
 
-    public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo, UserRepository userRepo) {
-        this.ingredientRepo = ingredientRepo;
+    @Autowired
+    EntityLinks entityLinks;
+
+    public DesignTacoController(TacoRepository tacoRepo) {
         this.tacoRepo = tacoRepo;
-        this.userRepo = userRepo;
     }
 
-    @ModelAttribute(name = "order")
-    public Order order(){
-        return new Order();
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos(){
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        return tacoRepo.findAll(page).getContent();
     }
 
-    @ModelAttribute(name = "taco")
-    public Taco taco(){
-        return new Taco();
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id){
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+        if(optTaco.isPresent()) return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping
-    public String showDesignForm(Model model, Principal principal) {
-
-        List<Ingredient> ingredients = new ArrayList<>();
-        ingredientRepo.findAll().forEach(i -> ingredients.add(i));
-
-        Type[] types = Type.values();
-        for (Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(),
-                    filterByType(ingredients, type));
-        }
-
-        String username = principal.getName();
-        User user = userRepo.findByUsername(username);
-        model.addAttribute("user", user);
-
-        return "design";
+    @PostMapping(consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco){
+        return tacoRepo.save(taco);
     }
 
-    private List<Ingredient> filterByType(
-            List<Ingredient> ingredients, Type type) {
-        return ingredients
-                .stream()
-                .filter(x -> x.getType().equals(type))
-                .collect(Collectors.toList());
-    }
 
-    @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
-        if(errors.hasErrors()){
-            return "design";
-        }
-
-        Taco saved = tacoRepo.save(design);
-        order.addDesign(saved);
-
-        return "redirect:/orders/current";
-    }
 }
--- chapter6/demo/src/main/java/tacos/web/OrderController.java
+++ chapter6/demo/src/main/java/tacos/web/OrderController.java
@@ -1,16 +1,12 @@
 package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.support.SessionStatus;
 import tacos.Order;
-import tacos.User;
 import tacos.data.OrderRepository;
-
-import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -24,33 +20,26 @@ public class OrderController {
         this.orderRepo = orderRepo;
     }
 
-    @GetMapping("/current")
-    public String orderForm(@AuthenticationPrincipal User user,
-                            @ModelAttribute Order order) {
-        if(order.getDeliveryName() == null) order.setDeliveryName(user.getFullname());
-        if(order.getDeliveryStreet() == null) order.setDeliveryStreet(user.getStreet());
-        if(order.getDeliveryCity() == null) order.setDeliveryCity(user.getCity());
-        if(order.getDeliveryState() == null) order.setDeliveryState(user.getState());
-        if(order.getDeliveryZip() == null) order.setDeliveryZip(user.getZip());
-
-        return "orderForm";
+    @PatchMapping(path = "/{orderId}", consumes = "application/json")
+    public Order patchOrder(@PathVariable("orderId") Long orderId,
+                            @RequestBody Order patch){
+        Order order = orderRepo.findById(orderId).get();
+        if(patch.getDeliveryName() != null) order.setDeliveryName(patch.getDeliveryName());
+        if(patch.getDeliveryStreet() != null) order.setDeliveryStreet(patch.getDeliveryStreet());
+        if(patch.getDeliveryCity() != null) order.setDeliveryCity(patch.getDeliveryCity());
+        if(patch.getDeliveryState() != null) order.setDeliveryState(patch.getDeliveryState());
+        if(patch.getDeliveryZip() != null) order.setDeliveryZip(patch.getDeliveryZip());
+        if(patch.getCcNumber() != null) order.setCcNumber(patch.getCcNumber());
+        if(patch.getCcExpiration() != null) order.setCcExpiration(patch.getCcExpiration());
+        if(patch.getCcCVV() != null) order.setCcCVV(patch.getCcCVV());
+        return orderRepo.save(order);
     }
 
-    @PostMapping
-    public String processOrder(@Valid Order order
-            , Errors errors
-            , SessionStatus sessionStatus
-            , @AuthenticationPrincipal User user
-    ) {
-        if (errors.hasErrors()) {
-            return "orderForm";
-        }
-
-        order.setUser(user);
-
-        orderRepo.save(order);
-        sessionStatus.setComplete();
-
-        return "redirect:/";
+    @DeleteMapping("/{orderId}")
+    @ResponseStatus(code= HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable("orderId") Long orderId){
+        try{
+            orderRepo.deleteById(orderId);
+        }catch (EmptyResultDataAccessException e) {}
     }
 }
