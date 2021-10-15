2a2189a4aaf57536fd7397685fde537b36d4486f
h3yon <khykhy1006@gmail.com>
2021-10-15 17:13:16 +0900

    Chapter6 Initial Setting

---
+++ chapter6/demo/src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,90 @@
+package tacos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**")
+                .access("permitAll")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .csrf();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // 4. custom
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
+
+        /* 1. 인메모리 방식
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password("{noop}password1")
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("user2")
+                .password("{noop}password2")
+                .authorities("ROLE_USER");
+        */
+
+        /* 2. LDAP 기반 사용자 스토어
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                // 스프링 시큐리티와 다른 데이터베이스(테이블이나 열의 이름이 다를 때)를 사용할 경우
+                .usersByUsernameQuery("select username, password, enabled, from users where username=?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username=?")
+                .passwordEncoder(new NoEncodingPasswordEncoder());
+        */
+
+        /* 3. ldap
+        auth.ldapAuthentication()
+                .userSearchBase("ou=people")
+                .userSearchFilter("(uid={0})")
+                .groupSearchBase("ou=groups")
+                .groupSearchFilter("member={0}")
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("userPassword");
+                *//*
+                .contextSource()
+                // 아래 둘 중 하나
+                .url("ldap://tacocloud.com:389/dc=tacoclud,dc=com")
+                .root("dc=tacocloud,dc=com")
+                //LDAP 데이터를 나타내는 표준화된 방법
+                .ldif("classpath:user.ldif")
+         */
+    }
+}
